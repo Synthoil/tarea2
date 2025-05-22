f161495452d5c5c433f43a26df0aa4311c9bb482
Benjamin <benjamidiaz2023@udec.cl>
2025-05-21 22:38:20 -0400

    Se agregan getter y setter

--- tarea2/src/main/java/Empleado.java
+++ tarea2/src/main/java/Empleado.java
@@ -10,4 +10,36 @@ class Empleado{
         this.nombre = nombre;
         this.correo = correo;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
